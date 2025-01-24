b561f290680415d175c9618738a9f2e563c0a311
marcoaga02 <marcoagatensi@gmail.com>
2025-01-24 19:36:59 +0100

    quite finished

--- test.sh
+++ test.sh
@@ -2,23 +2,22 @@
 
 # function to stress the RAM
 stress_ram() {
-    echo "Stressing RAM: for 20 seconds..."
-    stress --vm 4 --vm-bytes 2G --timeout 20s
-
+    echo "Stressing RAM: 90% with 2 threads for 20 seconds..."
+    stress-ng --vm 2 --vm-bytes 90% --timeout 20s
 }
 
 # function to stress the CPU
 stress_cpu() {
     local cores=$1
-    echo "Stressing CPU: $cores cores for 20 seconds..."
-    stress-ng --cpu $cores --timeout 20s
+    echo "Stressing CPU: $cores cores for 15 seconds..."
+    stress-ng --cpu $cores --timeout 15s
 }
 
 # Function to stress both CPU and RAM at the same time
 stress_mixed() {
     local cpu_cores=$1
-    echo "Stressing CPU ($cpu_cores cores) and RAM for 20 seconds..."
-    stress --cpu $cpu_cores --vm 4 --vm-bytes 2G --timeout 20s
+    echo "Stressing CPU and RAM (80%) for 15 seconds..."
+    stress-ng --cpu $cpu_cores --vm 1 --vm-bytes 80% --timeout 15s
 }
 
 echo "==============================="
@@ -41,7 +40,7 @@ stress_cpu $total_cores # stress all cores of the CPU
 echo "Cooling down for 10 seconds..." # Cooling down step after each stress cycle
 sleep 10
 
-stress_mixed $((total_cores / 2)) 
+stress_mixed $((total_cores / 2)) # Using half of the total number of cores, stress both RAM and CPU
 
 echo "==========================="
 echo "End of the stress cycle..."
